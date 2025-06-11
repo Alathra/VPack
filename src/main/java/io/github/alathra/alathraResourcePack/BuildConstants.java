2918a485a67dd2f5e9ac7298c8aedddc29015149
Schbem <131344591+Schbem@users.noreply.github.com>
2025-06-11 16:15:03 +0800

    fix: moved BuildConstants under project directory

--- src/main/java/io/github/alathra/alathraResourcePack/BuildConstants.java
+++ src/main/java/io/github/alathra/alathraResourcePack/BuildConstants.java
@@ -2,6 +2,5 @@ package io.github.alathra.alathraResourcePack;
 
 // The constants are replaced before compilation
 public class BuildConstants {
-
     public static final String VERSION = "${version}";
 }
